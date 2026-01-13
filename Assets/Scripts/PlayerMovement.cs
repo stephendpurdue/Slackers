ba6c2cc004c28bb784de2164bb9a77d3e70130f8
Stephen <stephendpurdue@gmail.com>
2026-01-13 12:28:52 +0000

    Initial commit - Slackers Unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public int facingDirection = 1;
+    public Rigidbody2D rb;
+    public Animator anim;
+
+
+    // Fixed Update is called 50x per frame.
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        if(horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
