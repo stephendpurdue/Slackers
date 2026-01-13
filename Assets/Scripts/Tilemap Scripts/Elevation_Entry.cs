43b7db4d06de7a7be507edeff0e76f71a9dd28f1
Stephen <stephendpurdue@gmail.com>
2026-01-13 13:16:38 +0000

    Updated collision checks.

--- Assets/Scripts/Tilemap Scripts/Elevation_Entry.cs
+++ Assets/Scripts/Tilemap Scripts/Elevation_Entry.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Elevation_Entry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Collider2D[] mountainColliders;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player") // Checks if entity entering the collider is the Player.
+        foreach (Collider2D mountain in mountainColliders)
+        {
+            mountain.enabled = false;
+        }
+
+        collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
     }
 }
