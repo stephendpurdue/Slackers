a9392a51acada0df28d2ab42b24fa30564c61be1
Stephen <stephendpurdue@gmail.com>
2026-01-15 11:48:04 +0000

    Implemented Enemy Combat on Collision

---
+++ Assets/Scripts/Enemy_Combat.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Combat : MonoBehaviour
+{
+    
+    public int damage = 1;
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+    }
+    
+}
