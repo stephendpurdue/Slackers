25560af2d55bbb997f21f2d47ceda5cdf11ada65
Stephen <stephendpurdue@gmail.com>
2026-01-15 11:10:36 +0000

    Many changes + PlayerHealth.cs

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public TMP_Text healthText;
+
+
+    private void Start()
+    {
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
